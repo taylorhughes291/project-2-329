ea0778382204ff0daec3f233c42120c07d36dc5c
Taylor Hughes <taylorhughes@Taylors-Air.lan>
2021-05-03 08:51:52 -0700

    integration fix

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom"
 const Nav = () => {
     return (
         <div className="nav">
-            <img className="logo" src="https://placeimg.com/200/100/any" />
+            <img className="logo" alt="wrappd logo" src="https://placeimg.com/200/100/any" />
             <ul className="options">
                 <li>About</li>
                 <Link to="/">
