43e44528891c0422be6e8c865eb0607d79008c59
Taylor Hughes <taylorhughes@Taylors-Air.lan>
2021-05-03 10:54:15 -0700

    added form input functionality to keyword builder

--- src/App.js
+++ src/App.js
@@ -3,14 +3,42 @@ import Nav from "./components/Nav"
 import KeywordBuilder from "./pages/KeywordBuilder"
 import FinalCart from "./pages/FinalCart"
 import {Route, Switch} from "react-router-dom"
+import {useState} from "react"
 
 function App() {
+  ////////////////////////
+  // Constants
+  ////////////////////////
+
+  // initialize state to capture person profile
+  const [person, setPerson] = useState({
+    name: "",
+    budget: "",
+    keywords: [],
+    keywordText: ""
+  })
+
+  ////////////////////////
+  // Functions
+  ////////////////////////
+
+  const handleSetPerson = (value) => {
+    setPerson(value)
+  }
+
+  ////////////////////////
+  // Render
+  ////////////////////////
+
   return (
     <div className="App">
       <Nav />
       <Switch>
         <Route exact path="/">
-          <KeywordBuilder />
+          <KeywordBuilder 
+            person={person}
+            setPerson={setPerson}
+          />
         </Route>
         <Route path="/finalcart">
           <FinalCart />
--- src/pages/KeywordBuilder.js
+++ src/pages/KeywordBuilder.js
@@ -2,13 +2,82 @@ import React from "react"
 import {Link} from "react-router-dom"
 import ProductCarousel from "../components/ProductCarousel"
 
-const KeywordBuilder = () => {
+const KeywordBuilder = (props) => {
+  ////////////////////////
+  // Constants
+  ////////////////////////
+
+
+
+  ////////////////////////
+  // Functions
+  ////////////////////////
+
+  const handleNameChange = (event) => {
+    let person = {
+        ...props.person,
+        name: event.target.value
+    }
+    props.setPerson(person)
+  }
+
+  const handleBudgetChange = (event) => {
+    let person = {
+        ...props.person,
+        budget: event.target.value
+    }
+    props.setPerson(person)
+  }
+
+
+  const handleKeywordChange = (event) => {
+    const keywordSplit = props.person.keywordText.split(",")
+
+    if (keywordSplit.length <= 3 || event.nativeEvent.inputType === "deleteContentBackward") {
+        if (event.nativeEvent.data === ",") {
+            let person = {
+                ...props.person,
+                keywordText: event.target.value,
+                keywords: keywordSplit
+            }
+            props.setPerson(person)
+        } else {
+            let person = {
+                ...props.person,
+                keywordText: event.target.value
+            }
+            props.setPerson(person)
+        }
+
+
+    }
+  }
+
+  ////////////////////////
+  // Render
+  ////////////////////////
+
     return (
         <div>
             <form>
-                <input type="text" placeholder="Person"></input>
-                <input type="number" placeholder="Budget"></input>
-                <input type="text" placeholder="Keywords"></input>
+                <input 
+                    type="text" 
+                    placeholder="Person"
+                    value={props.person.name}
+                    onChange={handleNameChange}
+                ></input>
+                <input 
+                    type="number" 
+                    placeholder="Budget"
+                    value={props.person.budget}
+                    onChange={handleBudgetChange}
+                ></input>
+                <input 
+                    type="text" 
+                    placeholder="Keywords"
+                    value={props.person.keywordText}
+                    onChange={handleKeywordChange}
+                ></input>
             </form>
             <ProductCarousel />
             <Link to="/finalcart">
