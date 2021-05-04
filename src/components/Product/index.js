996e69ec1829cb331a3ada7d9d3b8a570d49784e
Taylor Hughes <taylorhughes@Taylors-Air.lan>
2021-05-04 11:38:26 -0700

    rendered top products in product carousel

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,10 +1,31 @@
 import React from "react"
+import Card from "react-bootstrap/Card"
+import Button from "react-bootstrap/Button"
+import Col from "react-bootstrap/Col"
 
-const Product = () => {
+const Product = (props) => {
+    console.log(props)
+
+    const products = props.data.map((item, index) => {
+        return (
+            <Col>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={item.image} />
+                    <Card.Body>
+                        <Card.Text>
+                        {item.title}
+                        </Card.Text>
+                        <Card.Title>{`$${item.price.value}`}</Card.Title>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+        )
+    })
     return (
-        <div>
-            This is the Product Component
-        </div>
+        <>
+            {products}
+       </>
     )
 }
 
--- src/components/ProductCarousel/index.js
+++ src/components/ProductCarousel/index.js
@@ -1,28 +1,16 @@
 import React from "react"
-import Card from "react-bootstrap/Card"
-import Button from "react-bootstrap/Button"
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
+import Product from "../Product"
 
 const ProductCarousel = (props) => {
     return (
         <div className="product-carousel">
             <Container>
                 <Row>
-                    <Col>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="https://picsum.photos/286/180" />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
-                        </Card.Body>
-                    </Card>
-                    </Col>
+                    <Product 
+                        data={props.data}
+                    />
                 </Row>
             </Container>
 
