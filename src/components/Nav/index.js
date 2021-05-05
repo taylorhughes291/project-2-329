481bd68e2139bff675958a72b5ef8233039fe6d3
Taylor Hughes <taylorhughes@Taylors-Air.lan>
2021-05-05 13:18:41 -0700

    added hamburger icon to mobile menu

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,11 +1,28 @@
 import React from "react"
 import {Link} from "react-router-dom"
+import {useMediaQuery} from "react-responsive"
 
 const Nav = () => {
+
+    //////////////////////////
+    // Media Queries
+    //////////////////////////
+
+    const isTablet = useMediaQuery({ query: '(max-width: 1023px)' })
+    const isDesktop = useMediaQuery({ query: '(min-width: 1024px)' })
+    
+
+    //////////////////////////
+    // Render
+    //////////////////////////
+
     return (
-        <div >
+        <div className="nav">
             <img className="logo" alt="wrappd logo" src="https://placeimg.com/200/100/any" />
-            <ul className="options">
+            <div className="hamburger-cont">
+                <i className="fas fa-bars fa-3x"></i>
+            </div>
+            <ul className="options hidden">
                 <li>About</li>
                 <Link to="/">
                     <li>New Gift</li>
