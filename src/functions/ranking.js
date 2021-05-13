10630c232b24edbaa848f0e2b5f3c3fb6dac3506
Taylor Hughes <taylorhughes@Taylors-Air.lan>
2021-05-12 17:42:04 -0700

    fixed the selected items not changing class to selected

--- src/functions/ranking.js
+++ src/functions/ranking.js
@@ -25,29 +25,6 @@ const ranking = (arr, cost, keywords) => {
     })
 
 
-    // Now we need to eradicate duplicate values based off of ASIN.
-    for (let i = 0; i < sortedArray.length - 1; i += 1) {
-        for (let j = i + 1; j < sortedArray.length; j += 1) {
-            if (sortedArray[i].asin === sortedArray[j].asin) {
-                sortedArray[j].duplicate = true
-            }
-        }
-        if (!sortedArray[i].duplicate) {
-            sortedArray[i].duplicate = false
-        }
-    }
-
-    sortedArray = sortedArray.filter((item, index) => {
-        return (
-            !item.duplicate
-        )
-    })
-
-    for (const item of sortedArray) {
-        Object.assign(item, {selected: false})
-    }
-
-
     return sortedArray
 }
 
