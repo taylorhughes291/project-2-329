ddd6e9a1063985555fe01c96947ad647e656ae5f
Taylor Hughes <taylor hughes>
2021-07-08 17:28:22 -0700

    cleaned up some warnings

--- src/components/LandingModal/index.js
+++ src/components/LandingModal/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react"
+import React from "react"
 import {Modal} from "react-bootstrap"
 import KeywordInput from "./KeywordInput"
 import BudgetInput from "./BudgetInput"
