71700c6cf0bfc7ad40480df6161a3b2264ea0b07
Taylor Hughes <taylor hughes>
2021-11-28 22:00:59 -0800

    added associate linking

--- src/functions/getProducts.js
+++ src/functions/getProducts.js
@@ -1,7 +1,7 @@
 // This function handles the API call to Rainforest.
 const getProducts = async (searchTerm) => {
     const apiKey = process.env.REACT_APP_RAINFOREST_API_KEY
-    const associateId = "taylorhughe05-20"
+    const associateId = process.env.REACT_APP_AMAZON_ASSOCIATES_ID
     const url = `https://api.rainforestapi.com/request?api_key=${apiKey}&type=search&amazon_domain=amazon.com&associate_id=${associateId}&search_term=${searchTerm}`
     
     const response = await fetch(url)
